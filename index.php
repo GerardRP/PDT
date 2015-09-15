a62554436938d503c76caa087379419682711e21
GerardR <GerardRegot@Gerards-MacBook-Pro.local>
2015-09-14 19:28:00 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link type="text/css" rel="stylesheet" href="stylesheet.css" />
+
+<title>Polaris Dance Theatre | PDT</title>
+</head>
+<body>
+<img id="background-image" src="Images/background-wall.png" />
+
+<img src="Images/polarislogohorizontalnotag.png" id="PDT" /> 
+<div id="enter"><h2><a href="Home_page.php">ENTER</a></h2></div>
+
+</body>
+</html>
